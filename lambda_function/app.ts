bd2935ad6f7279ae629c1b3c222339b59f5b7f9b
James Dehart <jddehar@amazon.com>
2019-04-25 13:55:31 -0400

    Minor fixes
    Updated template and changed the TS helloworld function around.

--- lambda_function/app.ts
+++ lambda_function/app.ts
@@ -1,3 +1,9 @@
 export const handler = async (event: any = {}): Promise<any> => {
-    return {"Message" : "Hello Workshop!"}
+    var body = {Message : "Hello Workshop!"} 
+    var response = {
+        "statusCode": 200,
+        "body": JSON.stringify(body),
+        "isBase64Encoded": false
+    };
+    return response
 }
