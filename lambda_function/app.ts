ad341cad1730027ce216b73a6d861410950c0ce0
James Dehart <jddehar@amazon.com>
2019-04-30 13:49:21 -0400

    testing

--- lambda_function/app.ts
+++ lambda_function/app.ts
@@ -1,9 +1,28 @@
+
+import * as AWS from 'aws-sdk';
+import { S3 } from 'aws-sdk';
+
+
 export const handler = async (event: any = {}, context: any = {}): Promise<any> => {
-    var body = {Message : "Hello Workshop!"} 
-    var response = {
-        "statusCode": 200,
-        "body": JSON.stringify(body),
-        "isBase64Encoded": false
+    var s3 = new AWS.S3();
+    var bucketName = String(process.env.BUCKETNAME); 
+    var imageBuffer = new Buffer(event.encoded_image, 'base64');
+
+    const s3Params = {
+        Bucket: bucketName,
+        Key: event.filename,
+        Body: imageBuffer    
     };
-    return response
+
+    s3.upload(s3Params, function(err: Error, data: any) {
+        console.log(err);
+        if(err) { return console.log(err) }
+        console.log(`Uploaded to ${data.Location}`);
+        var body = { imageURL: data.Location}
+        return {
+            statusCode: 200,
+            isBase64Encoded: false,
+            body: JSON.stringify(body)
+        }
+    })
 }
